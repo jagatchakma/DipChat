a652acfcb1a8f76045386dce8179caac3d5fc33d
jagat mettra chakma <mettradbd@gmail.com>
2025-11-29 02:15:48 +0600

    arcjet limiting implemented

--- DipChat_BackEnd/src/routes/auth.router.js
+++ DipChat_BackEnd/src/routes/auth.router.js
@@ -1,7 +1,10 @@
 import express from "express";
 import { signup, login, logout, update } from "../controllers/auth.controller.js";
+import { arcjetProtection } from "../models/arcjet.middleware.js";
 
 const router = express.Router();
+
+router.use(arcjetProtection)
 
 router.post("/signup", signup);
 
@@ -10,5 +13,10 @@ router.post("/login", login);
 router.post("/logout", logout);
 
 router.post("/update", update);
+
+// testing
+router.get("/test", (req, res) => {
+    res.status(200).json({message: "test for limit"})
+})
 
 export default router;
