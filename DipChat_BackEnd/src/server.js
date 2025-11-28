b538b606a335f38f61bcc769b27ee29770b9af03
jagat mettra chakma <mettradbd@gmail.com>
2025-11-29 04:25:57 +0600

    update problem solver

--- DipChat_BackEnd/src/server.js
+++ DipChat_BackEnd/src/server.js
@@ -11,7 +11,7 @@ import cookieParser from "cookie-parser";
 
 // middleware
 app.use(express.json()); // req.body
-app.use(cookieParser);
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
